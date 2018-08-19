994ef85c6b377fce777135ef53046b52c01f13d7
begimov <begimov@gmail.com>
2018-08-19 08:59:49 +0300

    basic repository infrastructure added

---
+++ app/Repositories/Eloquent/EloquentRepositoryAbstract.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Repositories\Eloquent;
+
+use App\Repositories\Exceptions\NoEntityDefined;
+use App\Repositories\Contracts\RepositoryInterface;
+use App\Repositories\Contracts\Criteria\CriteriaInterface;
+
+abstract class EloquentRepositoryAbstract implements RepositoryInterface, CriteriaInterface
+{
+    protected $entity;
+
+    public function __construct()
+    {
+        $this->entity = $this->resolveEntity();
+    }
+
+    public function get()
+    {
+        return $this->entity->get();
+    }
+
+    public function withCriteria(array $criteria)
+    {
+        foreach ($criteria as $criterion) {
+            $this->entity = $criterion->apply($this->entity);
+        }
+        return $this;
+    }
+
+    protected function resolveEntity()
+    {
+        if (!method_exists($this, 'entity')) {
+            throw new NoEntityDefined();
+        }
+        return app()->make($this->entity());
+    }
+}
---
+++ app/Repositories/Eloquent/Products/EloquentCategoryRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Repositories\Eloquent\Products;
+
+use App\Repositories\Eloquent\EloquentRepositoryAbstract;
+use App\Repositories\Contracts\Products\CategoryRepository;
+use App\Models\Products\Category;
+
+class EloquentCategoryRepository extends EloquentRepositoryAbstract implements CategoryRepository
+{
+    public function entity()
+    {
+        return Category::class;
+    }
+}
