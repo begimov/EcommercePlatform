a93fd2465c6ea18141d11ef3084420f2ece2d57a
begimov <begimov@gmail.com>
2018-09-01 13:18:54 +0300

    ProductScopeTest created

---
+++ tests/Feature/Products/ProductScopeTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Feature\Products;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ProductScopeTest extends TestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
