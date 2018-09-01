5e18b38ee1be45aeb1161c95bada41b841e679b4
begimov <begimov@gmail.com>
2018-09-01 09:06:44 +0300

    ProductShowTest created

---
+++ tests/Feature/Products/ProductShowTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature\Products;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ProductShowTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
