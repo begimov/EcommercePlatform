da01442cd843ab9fdba2234f2fbf3e4283f986b3
begimov <begimov@gmail.com>
2018-09-01 13:17:32 +0300

    Product's test_has_categories added

--- tests/Unit/Models/Products/ProductTest.php
+++ tests/Unit/Models/Products/ProductTest.php
@@ -6,18 +6,30 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-use App\Models\Products\Product;
+use App\Models\Products\{
+    Category,
+    Product
+};
 
 class ProductTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
     public function test_retrieved_by_slug()
     {
         $product = new Product();
+
         $this->assertEquals($product->getRouteKeyName(), 'slug');
+    }
+
+    public function test_has_categories()
+    {
+        $product = factory(Product::class)->create();
+
+        $product->categories()->save(
+
+            $category = factory(Category::class)->create()
+
+        );
+
+        $this->assertEquals($product->categories->first()->slug, $category->slug);
     }
 }
