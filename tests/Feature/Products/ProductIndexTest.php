da72791a93462147bf3cf5eb90f5753fa1d83a3a
begimov <begimov@gmail.com>
2018-09-01 08:47:13 +0300

    ProductIndexTest implemented

--- tests/Feature/Products/ProductIndexTest.php
+++ tests/Feature/Products/ProductIndexTest.php
@@ -6,10 +6,27 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
+use App\Models\Products\Product;
+
 class ProductIndexTest extends TestCase
 {
     public function test_returns_products_collection()
     {
-        //
+        $product = factory(Product::Class)->create();
+
+        $this->json('GET', 'api/products')
+            ->assertJsonFragment([
+                'slug' => $product->slug
+            ]);
+    }
+
+    public function test_contains_pagination_data()
+    {
+        $this->json('GET', 'api/products')
+            ->assertJsonStructure([
+                'data',
+                'links' => ['first'],
+                'meta' => ['from']
+            ]);
     }
 }
