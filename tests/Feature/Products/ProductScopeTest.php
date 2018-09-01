3fc83b2bbd5e2e390d819ff7f673d43aa23d71d2
begimov <begimov@gmail.com>
2018-09-01 13:28:45 +0300

    test_scope_by_category implemented

--- tests/Feature/Products/ProductScopeTest.php
+++ tests/Feature/Products/ProductScopeTest.php
@@ -6,10 +6,31 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
+use App\Models\Products\{
+    Category,
+    Product
+};
+
 class ProductScopeTest extends TestCase
 {
-    public function testExample()
+    public function test_scope_by_category()
     {
-        $this->assertTrue(true);
+        $product = factory(Product::class)->create();
+
+        $product->categories()->save(
+
+            $category = factory(Category::class)->create()
+
+        );
+
+        $anotherProduct = factory(Product::class)->create();
+
+        $anotherCategory = factory(Category::class)->create();
+
+        $this->json('GET', "api/products?category={$category->slug}")
+            ->assertJsonCount(1, 'data');
+
+        $this->json('GET', "api/products?category={$anotherCategory->slug}")
+            ->assertJsonCount(0, 'data');
     }
 }
