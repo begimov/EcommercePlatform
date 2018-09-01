8bb4de0eab5134b48e9b0cf21e8f3269d59616b5
begimov <begimov@gmail.com>
2018-09-01 08:37:48 +0300

    ProductIndexTest created

--- app/Http/Controllers/Products/ProductController.php
+++ app/Http/Controllers/Products/ProductController.php
@@ -16,7 +16,7 @@ class ProductController extends Controller
 
     public function index()
     {
-        $products = $this->products->get();
+        $products = $this->products->paginate(10);
         
         return ProductIndexResource::collection($products);
     }
---
+++ tests/Feature/Products/ProductIndexTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Feature\Products;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ProductIndexTest extends TestCase
+{
+    public function test_returns_products_collection()
+    {
+        //
+    }
+}
