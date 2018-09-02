482ebf2d4c20d3c4e5bd9619c356548394d0998b
begimov <begimov@gmail.com>
2018-09-02 12:18:23 +0300

    basic prices tests added to ProductVariationTest

--- tests/Unit/Models/Products/ProductVariationTest.php
+++ tests/Unit/Models/Products/ProductVariationTest.php
@@ -11,6 +11,8 @@ use App\Models\Products\{
     ProductVariation
 };
 
+use App\Services\App\Money;
+
 class ProductVariationTest extends TestCase
 {
     public function test_belongs_to_product()
@@ -26,5 +28,28 @@ class ProductVariationTest extends TestCase
         );
 
         $this->assertEquals($variation->product->id, $product->id);
+    }
+
+    public function test_returns_money_instance()
+    {
+        $product = factory(Product::class)->create();
+
+        $productVariation = factory(ProductVariation::class)->create([
+            'product_id' => $product->id
+        ]);
+
+        $this->assertInstanceOf(Money::class, $productVariation->price);
+    }
+
+    public function test_returns_formatted_price()
+    {
+        $product = factory(Product::class)->create();
+
+        $productVariation = factory(ProductVariation::class)->create([
+            'product_id' => $product->id,
+            'price' => 12000
+        ]);
+
+        $this->assertEquals("120,00 ₽", $productVariation->formattedPrice);
     }
 }
