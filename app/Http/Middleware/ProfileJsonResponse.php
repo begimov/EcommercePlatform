42aa182ad2617cdc22f63cb4cc685eec90f6376f
begimov <begimov@gmail.com>
2018-09-22 14:37:14 +0300

    ProfileJsonResponse middleware created and registered

---
+++ app/Http/Middleware/ProfileJsonResponse.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\JsonResponse;
+
+class ProfileJsonResponse
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        if ($response instanceof JsonResponse && app('debugbar')->isEnabled()) {
+            dd('OK');
+        }
+
+        return $response;
+    }
+}
