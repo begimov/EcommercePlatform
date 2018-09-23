8bbb26e4acb80abd43851eca5dcfdcc31a221be4
begimov <begimov@gmail.com>
2018-09-23 09:46:04 +0300

    register api route created

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,10 @@ use Illuminate\Http\Request;
 Route::resource('categories', 'Products\CategoryController');
 
 Route::resource('products', 'Products\ProductController');
+
+Route::group(['prefix' => 'auth', 'namespace' => 'Auth'], function() {
+
+    Route::post('register', 'RegisterController@register');
+    
+});
+
