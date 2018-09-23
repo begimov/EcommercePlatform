3da5c9b1006cc34ca1de71d1bf8897f097046cfc
begimov <begimov@gmail.com>
2018-09-23 10:01:09 +0300

    PrivateUserResource implemented and used to respond with newly created user data

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,11 +5,14 @@ namespace App\Http\Controllers\Auth;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\PrivateUserResource;
 
 class RegisterController extends Controller
 {
     public function register(Request $request)
     {
         $user = User::create($request->only('name', 'email', 'password'));
+
+        return new PrivateUserResource($user);
     }
 }
