389a3a0bf5c900a1130460100aa579daca9caa78
begimov <begimov@gmail.com>
2018-09-29 07:57:28 +0300

    Login form request created, rules implemented

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Resources\PrivateUserResource;
 
 class LoginController extends Controller
 {
-    public function login(Request $request)
+    public function login(LoginRequest $request)
     {
         if (!$token = auth()->attempt($request->only('email', 'password'))) {
 
