7fcda9e8c6a93aff260f0e225aea09dd3e9be202
begimov <begimov@gmail.com>
2018-09-29 07:51:35 +0300

    basic login  user implemented

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,11 +4,23 @@ namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\PrivateUserResource;
 
 class LoginController extends Controller
 {
-    public function login()
+    public function login(Request $request)
     {
-        return 1;
+        if (!$token = auth()->attempt($request->only('email', 'password'))) {
+
+            return 'error';
+
+        }
+
+        return (new PrivateUserResource($request->user()))
+            ->additional([
+                'meta' => [
+                    'token' => $token,
+                ]
+            ]);
     }
 }
