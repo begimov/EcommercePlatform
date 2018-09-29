14f83b2938309c883552c57b56e5d4f459efb318
begimov <begimov@gmail.com>
2018-09-29 08:45:18 +0300

    responding with authenticated user's private resource

--- app/Http/Controllers/Auth/MeController.php
+++ app/Http/Controllers/Auth/MeController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\PrivateUserResource;
 
 class MeController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return 1;
+        return new PrivateUserResource($request->user());
     }
 }
