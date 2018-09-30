f5189dcef8a44660f5d65ad6ae0962d4eb222e19
begimov <begimov@gmail.com>
2018-09-30 13:28:00 +0300

    adding products to cart abstrcated away via service Cart class

--- app/Http/Controllers/Carts/CartController.php
+++ app/Http/Controllers/Carts/CartController.php
@@ -5,23 +5,21 @@ namespace App\Http\Controllers\Carts;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Carts\CartStoreRequest;
+use App\Services\App\Cart;
 
 class CartController extends Controller
 {
+    protected $cart;
+
+    public function __construct(Cart $cart)
+    {
+        $this->cart = $cart;
+    }
+
     public function store(CartStoreRequest $request)
     {
-        $products = $this->processProducts($request->products);
-
-        $request->user()->cart()->syncWithoutDetaching($products);
+        $this->cart->add($request->products);
     }
 
-    protected function processProducts(array $products)
-    {
-        return array_reduce($products, function($result, $product) {
-            $result[$product['id']] = [
-                'quantity' => $product['quantity']
-            ];
-            return $result;
-        }, []);
-    }
+    
 }
--- app/Services/App/Cart.php
+++ app/Services/App/Cart.php
@@ -12,4 +12,20 @@ class Cart
     {
         $this->user = $user;
     }
+
+    public function add(array $products)
+    {
+        $this->user->cart()
+            ->syncWithoutDetaching($this->processProducts($products));
+    }
+    
+    protected function processProducts(array $products)
+    {
+        return array_reduce($products, function($result, $product) {
+            $result[$product['id']] = [
+                'quantity' => $product['quantity']
+            ];
+            return $result;
+        }, []);
+    }
 }
