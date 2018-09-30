cc352bfd2b681f32527bef9a7f868a416842d40a
begimov <begimov@gmail.com>
2018-09-30 12:52:47 +0300

    CartStoreRequest created, validation rules implemented

--- app/Http/Controllers/Carts/CartController.php
+++ app/Http/Controllers/Carts/CartController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers\Carts;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Carts\CartStoreRequest;
 
 class CartController extends Controller
 {
-    public function store()
+    public function store(CartStoreRequest $request)
     {
         return 1;
     }
