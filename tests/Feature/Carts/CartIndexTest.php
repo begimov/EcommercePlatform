b9b07ba0b7aa9ea161db27913f8213a0309013ac
begimov <begimov@gmail.com>
2018-10-12 05:25:45 +0300

    CartIndexTest created

---
+++ tests/Feature/Carts/CartIndexTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature\Carts;
+
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Products\ProductVariation;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CartIndexTest extends TestCase
+{
+    public function test_fails_if_user_not_authenticated()
+    {
+        $this->json('GET', 'api/carts')
+            ->assertStatus(401);
+    }
+
+    public function test_returns_list_of_products()
+    {
+        $user = factory(User::class)->create();
+
+        $this->jsonAs($user, 'GET', 'api/carts')
+            ->assertJsonCount(0, 'data.products');
+
+        $user->cart()->attach(
+            factory(ProductVariation::class)->create()
+        );
+
+        $this->jsonAs($user, 'GET', 'api/carts')
+            ->assertJsonCount(1, 'data.products');
+    }
+}
